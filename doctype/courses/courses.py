f44022dd8f29aa76b7aff6a2557a3d546dd99755
Abhinav Jain <58175317+abhi-146@users.noreply.github.com>
2024-05-06 15:26:33 +0530

    Delete doctype directory

--- doctype/courses/courses.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Abhinav jain and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class courses(Document):
-	pass
